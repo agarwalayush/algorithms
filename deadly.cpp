fa11b81eb7006fa94f61dc2912f4b93a09f4ed6e
Ayush Agarwal <agarwal.ayush9@gmail.com>
2015-08-25 12:11:13 +0530

    final code with time and multiple test cases

--- deadly.cpp
+++ deadly.cpp
@@ -3,12 +3,18 @@
 #include<cstdlib>
 #include<utility>
 #include<algorithm>
+#include<ctime>
+#include<time.h>
 using namespace std;
 
 #define x1 first.first
 #define x2 first.second
 #define y second
 long long int total=0;
+pair< pair<double, double>, double > b[10000000]; 
+pair<double, double> y[10000000]; 
+pair<double, double> v[10000000];
+
 typedef struct node {
     double x;
     int subtree_size;
@@ -32,7 +38,6 @@ node* nodeCreator(double key){
 
 void insert_key(double key){
     node *parent,*root;
-    //cout<<"inserting key "<<key<<endl;
     parent=root=bst;
     while(root!= NULL){
         parent=root;
@@ -42,12 +47,10 @@ void insert_key(double key){
         else if(key < root->x)
             root = root->left;
         else{
-            //cout<<root->subtree_size<<endl;
             return ;
         }
 
     }
-    //cout<<parent->subtree_size<<endl;
     if(parent != NULL){
         if(key > parent->x)
             parent->right = nodeCreator(key);
@@ -58,7 +61,6 @@ void insert_key(double key){
 }
 
 void delete_key(double key){
-    //cout<<"deleting key "<<key<<endl;
     node* root=bst;
     while(root->x != key){
         root->subtree_size--;
@@ -81,9 +83,7 @@ int greater_than(double x){
  node *root=bst;
     int sum=0;
     while(root != NULL){
-        //cout<<"inside the loop of greater_than "<<root->x<<" "<<x<<endl;
         if(x >= root->x){
-            //if(x == root->x)sum+=size(root) - size(root->left);
             root=root->right;
         }
         else {
@@ -98,9 +98,7 @@ int greater_(double x){
  node *root=bst;
     int sum=0;
     while(root != NULL){
-        //cout<<"inside the loop of greater_than "<<root->x<<" "<<x<<endl;
         if(x > root->x){
-            //if(x == root->x)sum+=size(root) - size(root->left);
             root=root->right;
         }
         else {
@@ -112,7 +110,6 @@ int greater_(double x){
 }
 
 int search(double x_first, double x_second){
-    //cout<<greater_<<" "<<greater_than<<endl;
     return greater_(x_first) - greater_than(x_second);
 }
 
@@ -122,53 +119,72 @@ int findtotal(int n,pair<pair<double,double>, double > *b, pair<double,double> *
     while(i<n){
         while(j<n && b[i].y >= y[j].first){
             insert_key(y[j].second);
-          //  cout<<"key is inserted and blue value was "<<b[i].y<<endl;
             j++;
         }
         while(k<n && b[i].y > v[k].first){
             delete_key(v[k].second);
             k++;
-          //  cout<<"key deleted"<<endl;
         }
         temp=search(b[i].x1,b[i].x2);
         total+=temp;
         i++;
-        //cout<<"incrementing blue and search count was "<<temp<<endl;
     }
 }
 
-
+int delete_tree(node* root){
+    if(root == NULL) return 0;
+    if(root->left != NULL)
+        delete_tree(root->left);
+    if(root->right != NULL)
+        delete_tree(root->right);
+    delete(root);
+    root=NULL;
+    return 0;
+}
 
 int main(){
-//    srand (static_cast <unsigned> (time(0))); 
+    srand (static_cast <unsigned> (time(0))); 
     std::ios_base::sync_with_stdio(false);
-    int n,i;
-    double p,q,r;
+    int n,i,t=0;
+    clock_t begin,e;
+    time_t start, end;
+    double p,q,r,total_time=0;
     cin>>n;
-    pair< pair<double, double>, double > b[n]; 
-    pair<double, double> y[n]; 
-    pair<double, double> v[n];
-    for(i=0; i<n; i++){
-        cin>>p>>q>>r;
-        /*p = ((double) rand()/(double)RAND_MAX);
-        q = ((double) rand()/(double)RAND_MAX);
-        r = ((double) rand()/(double)RAND_MAX);*/
-        b[i].x1 = min(p,q);
-        b[i].x2 = max(p,q);
-        b[i].y = r;
+    int testcase=10000000/n;
+    long long answer=0;
+    while(t < testcase){
+        t++;
+        for(i=0; i<n; i++){
+            p = ((double) rand()/(double)RAND_MAX);
+            q = ((double) rand()/(double)RAND_MAX);
+            r = ((double) rand()/(double)RAND_MAX);
+            b[i].x1 = min(p,q);
+            b[i].x2 = max(p,q);
+            b[i].y = r;
+        }
+        for(i=0; i<n; i++){
+            p = ((double) rand()/(double)RAND_MAX);
+            q = ((double) rand()/(double)RAND_MAX);
+            r = ((double) rand()/(double)RAND_MAX);
+            y[i].first = min(p,q);
+            v[i].first = max(p,q);
+            v[i].second = y[i].second = r;
+        }
+        time(&start);
+        //begin = clock();
+        sort(b,b+n, blueFunction);
+        sort(y,y+n,redFunction);
+        sort(v,v+n, redFunction);
+        findtotal(n,b,y,v);
+        time(&end);
+        //e = clock();
+        total_time+=difftime(end,start);
+        answer+=total;
+        delete_tree(bst);
+        bst=NULL;
+        total=0;
     }
-    for(i=0; i<n; i++){
-        cin>>p>>q>>r;
-        /*p = ((double) rand()/(double)RAND_MAX);
-        q = ((double) rand()/(double)RAND_MAX);
-        r = ((double) rand()/(double)RAND_MAX);*/
-        y[i].first = min(p,q);
-        v[i].first = max(p,q);
-        v[i].second = y[i].second = r;
-    }
-    sort(b,b+n, blueFunction);
-    sort(y,y+n,redFunction);
-    sort(v,v+n, redFunction);
-    findtotal(n,b,y,v);
-    cout<<total<<endl;
+    cout<<answer*(1.0)/testcase<<endl;
+    cout<<"time elapsed "<<total_time/testcase<<endl;
 }
+
